1b4203cce4cb7f01318ae1bc190283aac9437bfc
Mbourza <46419852+Mbourza@users.noreply.github.com>
2025-10-29 04:50:52 -0700

    Update e_config.php

--- dashboard/controllers/config/e_config.php
+++ dashboard/controllers/config/e_config.php
@@ -5,8 +5,8 @@ define('IMAP_PORT', 993);
 define('IMAP_SSL', true);
 define('SMTP_HOST', 'smtp.hostinger.com');
 define('SMTP_PORT', 587);
-define('EMAIL_USERNAME', 'support@obgecom.com');
-define('EMAIL_PASSWORD', 'Obg@123456');
+define('EMAIL_USERNAME', '');
+define('EMAIL_PASSWORD', '');
 
 // Dossiers IMAP
 define('INBOX_FOLDER', 'INBOX');
